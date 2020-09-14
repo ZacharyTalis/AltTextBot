1867485a6c409fe058fe96a526b402d76e4ddf09
Glossawy <4606234+Glossawy@users.noreply.github.com>
2020-09-14 03:34:07 -0400

    stop trying to hack completable future

--- src/main/java/com/zacharytalis/alttextbot/EntryPoint.java
+++ src/main/java/com/zacharytalis/alttextbot/EntryPoint.java
@@ -5,19 +5,13 @@ import com.zacharytalis.alttextbot.commands.CommandRegistry;
 import com.zacharytalis.alttextbot.commands.impl.AltCommand;
 import com.zacharytalis.alttextbot.commands.impl.HelpCommand;
 import com.zacharytalis.alttextbot.commands.impl.PingCommand;
-import com.zacharytalis.alttextbot.concurrent.BotThreadFactory;
 
 import static com.zacharytalis.alttextbot.utils.Ref.TOKEN_VAR;
 
 public class EntryPoint {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         var token = System.getenv(TOKEN_VAR);
         var cmds = new CommandRegistry();
-
-        System.setProperty(
-            "java.util.concurrent.ForkJoinPool.common.threadFactory",
-            BotThreadFactory.class.getName()
-        );
 
         if (token == null)
             throw new NullPointerException("No environment variable: BOT_TOKEN");
