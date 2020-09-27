0a48a7fdbaf9deb5623306ae4ac9fd19016fc19e
Glossawy <4606234+Glossawy@users.noreply.github.com>
2020-09-26 22:38:34 -0400

    Add new publish scripts for bash and ps1

---
+++ scripts/publish.ps1
@@ -0,0 +1,26 @@
+param([string] $Version)
+
+if ( -not ($Version)) {
+    throw "You must supply a version either as the first argument, or via -Version. Ex: 1.2-beta"
+    exit 1
+}
+
+function record {
+    param(
+        [parameter(ValueFromRemainingArguments)]
+        [string[]] $Passthrough
+    )
+
+    Write-Host "+ $Passthrough"
+    Invoke-Expression "$Passthrough"
+}
+
+record .\gradlew --console verbose assemble
+
+record docker build -t alt-text-bot .
+
+record docker tag alt-text-bot "glossawy/alt-text-bot:$Version"
+record docker tag alt-text-bot glossawy/alt-text-bot:current
+
+record docker push "glossawy/alt-text-bot:$Version"
+record docker push glossawy/alt-text-bot:current
---
+++ scripts/publish.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+set -Ee
+
+if [ $# -lt 1 ]; then
+  {
+    echo "Missing version argument"
+    echo "---"
+    echo "Usage: ./publish.sh <VERSION_NAME>"
+  } >&2
+
+  exit 1
+fi
+
+version="$1"
+
+record() {
+  echo "+ $*"
+  "$@"
+}
+
+
+record ./gradlew assemble
+
+record docker build -t alt-text-bot .
+record docker tag alt-text-bot glossawy/alt-text-bot:"$version"
+record docker tag alt-text-bot glossawy/alt-text-bot:current
+
+record docker push glossawy/alt-text-bot:"$version"
+record docker push glossawy/alt-text-bot:current
