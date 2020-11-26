d2ae6e2cd2965167729e8f1014b119c10d39819e
Glossawy <4606234+Glossawy@users.noreply.github.com>
2020-11-26 18:30:01 -0500

    improve publish scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,29 +1,95 @@
 #!/usr/bin/env zsh
-set -Ee
+set -Eeuo pipefail
 
-if [ $# -lt 1 ]; then
+show-help() {
+  echo "Missing version argument"
+  echo "---"
+  echo "Usage: ./publish.sh <arguments>"
+  echo "Required Arguments:"
+  echo "  -a|--author  <author >  : Docker author tag, e.g. 'glossawy' in glossawy/image-name:3.2.1"
+  echo "  -t|--tag     <app-tag>  : Local docker tag for app, e.g. 'image-name' in glossawy/image-name:3.2.1"
+  echo "  -v|--version <version>  : App version, e.g. '3.2.1' in glossawy/image-name:3.2.1"
+  echo
+  echo "Options:"
+  echo "  -h|--help : print this help text and exit"
+}
+
+[[ $# -eq 0 ]] && show-help && exit 1
+
+error-unknown-flag() {
   {
-    echo "Missing version argument"
-    echo "---"
-    echo "Usage: ./publish.sh <VERSION_NAME>"
-  } >&2
+    echo "error: $1 is not a valid flag"
+    show-help
+  } &>2
 
   exit 1
-fi
+}
 
-version="$1"
+error-missing-argument() {
+  {
+    echo "error: missing value for $1"
+    show-help
+  } &>2
+
+  exit 2
+}
+
+author=false
+local_tag=false
+version=false
+
+while [[ $# -gt 0 ]]; do
+  flag="$1"
+
+  case "$flag" in
+  -a | --author)
+    author="$2"
+    shift
+    shift
+    ;;
+  -t | --tag)
+    local_tag="$2"
+    shift
+    shift
+    ;;
+  -v | --version)
+    version="$2"
+    shift
+    shift
+    ;;
+  -h | --help)
+    show-help
+    exit 0
+    ;;
+  *)
+    error-unknown-flag "$flag"
+    ;;
+  esac
+done
+
+[[ "$author" == false ]] && error-missing-argument '--author'
+[[ "$version" == false ]] && error-missing-argument '--version'
+[[ "$local_tag" == false ]] && error-missing-argument '--name'
 
 record() {
   echo "+ $*"
   "$@"
 }
 
+docker_hub_tag="${author}/${local_tag}"
 
-record ./gradlew assemble
+echo "App        : $local_tag"
+echo "Author     : $author"
+echo "Docker Tag : $docker_hub_tag"
+echo "Version    : $version"
+echo
 
-record docker build -f ./Dockerfile -t alt-text-bot --build-arg version="$version" ./build/libs
-record docker tag alt-text-bot glossawy/alt-text-bot:"$version"
-record docker tag alt-text-bot glossawy/alt-text-bot:current
+record ./gradlew --console verbose assemble
 
-record docker push glossawy/alt-text-bot:"$version"
-record docker push glossawy/alt-text-bot:current
+record docker build -f ./Dockerfile -t "$local_tag" --build-arg version="$version" ./build/libs
+
+record docker tag "$local_tag" "$docker_hub_tag":"$version"
+record docker tag "$local_tag" "$docker_hub_tag":current
+
+record docker push "$docker_hub_tag":"$version"
+record docker push "$docker_hub_tag":current
